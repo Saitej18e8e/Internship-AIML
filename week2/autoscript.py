4bc57a95b6ec3df3ceeb5e0b84845445fcd00faa
Saitej <saitejsunilwadekar@gmail.com>
2026-06-29 19:42:57 +0530

    Remove virtual environment

---
+++ week2/autoscript.py
@@ -0,0 +1,17 @@
+#import os
+
+# print(os.getcwd())
+# print(os.listdir())
+
+# # os.mkdir("Day8")
+# os,rename("Day10,DayEight")
+# # os.rmdir("DayEight")
+
+# os.rmdir("Day9")
+
+
+import shutil #used for file opperation
+
+shutil.copy(), shutil.move() os.remove()
+shutil,copytree(),shutil.rmtree()
+
